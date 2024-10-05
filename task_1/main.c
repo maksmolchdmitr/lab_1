11e42dc76c717f3e2e1694d463f41a5439bd5acd
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-10-05 12:47:08 +0300

    first version

--- task_1/main.c
+++ task_1/main.c
@@ -3,25 +3,28 @@
 #include <stdlib.h>
 
 int thread_count;
-int m;
-int n;
-int* matrix;
-int* vector;
-int* result;
+int trials_count;
+int trials_on_thread;
+int* results;
+
+double randPoint(){
+    double max = 1.0;
+    double min = -1.0;
+    double range = (max - min);
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
 
 void* routine(void* rank){
 	long my_rank = (long)rank;
-	int my_rows_count = m / thread_count;
-	int my_first_row = my_rank * my_rows_count;
-	int my_last_row = (my_rank+1) * my_rows_count;
-
-	for (long i = my_first_row; i < my_last_row; ++i)
-	{
-		for (long j = 0; j < n; ++j)
-		{
-			result[i] += matrix[i*n+j] * vector[j];
-		}
-	}
+    for(int i = 0; i < trials_on_thread; i++){
+        double x = randPoint();
+        double y = randPoint();
+        printf("%f, %f\n", x, y);
+        if(x * x + y * y < 1){
+            results[my_rank]++;
+        }
+    }
 	return NULL;
 }
 
@@ -30,61 +33,33 @@ int main(int argc, char const *argv[])
 	printf("Hello from main thread!\n");
 
 	thread_count = strtol(argv[1], NULL, 10);
-	m = strtol(argv[2], NULL, 10);
-	n = strtol(argv[3], NULL, 10);
+	trials_count = strtol(argv[2], NULL, 10);
+    trials_on_thread = trials_count/thread_count;
 
-	pthread_t* thread_handles = malloc(thread_count * sizeof(pthread_t));
+	pthread_t* threads = malloc(thread_count * sizeof(pthread_t));
 
-	matrix = malloc(n*m*sizeof(int));
-	vector = malloc(n*sizeof(int));
-	result = calloc(m, sizeof(int));
-
-	for (long i = 0; i < n*m; ++i)
-	{
-		matrix[i] = rand() % 10;
-	}
-
-	printf("Matrix\n");
-	for (int i = 0; i < m; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			printf("%d ", matrix[i*n+j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-
-	for (long i = 0; i < n; ++i)
-	{
-		vector[i] = rand() % 10;
-	}
-
-	printf("Vector\n");
-	for (int i = 0; i < n; ++i)
-	{
-		printf("%d ", vector[i]);
-	}
-	printf("\n");
+	results = calloc(thread_count, sizeof(int));
 
 	for(long i = 0; i < thread_count; i++){
-		pthread_create(&thread_handles[i], NULL, routine, (void*)i);
+		pthread_create(&threads[i], NULL, routine, (void*)i);
 	}
 
 	for (long i = 0; i < thread_count; ++i)
 	{
-		pthread_join(thread_handles[i], NULL);
-		// pthread_detach(thread_handles[i]);
+		pthread_join(threads[i], NULL);
 	}
 
 	printf("Result\n");
-	for (int i = 0; i < m; ++i)
+    double pi = 0;
+	for (int i = 0; i < thread_count; ++i)
 	{
-		printf("%d ", result[i]);
+        pi += results[i];
 	}
+    pi /= trials_count;
+    pi *= 4;
+    printf("PI = %f", pi);
 
-	free(thread_handles);
-	free(result);
-	free(matrix);
+	free(threads);
+	free(results);
 	return 0;
 }
