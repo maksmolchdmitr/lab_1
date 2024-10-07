941fbf372064098beb133305a78ac44485013e47
Nikita Kuritsyn <martyz89@yandex.ru>
2024-10-07 21:06:45 +0300

    Add task_2 correct implementation

--- task_2/main.c
+++ task_2/main.c
@@ -1,8 +1,15 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "timer.h"
 #include "math.h"
+
+double X_MIN = -2.0;
+double X_MAX = 1.0;
+double Y_MIN = -1.0;
+double Y_MAX = 1.0;
+long ITERATION_COUNT = 4000;
 
 struct Point {
     double x;
@@ -11,40 +18,10 @@ struct Point {
 
 int thread_count;
 unsigned long point_count;
+unsigned long cur_point_count;
 struct Point* result;
-long y_count;
-long x_count;
-double* ys;
-double* xs;
-
-void* routine(void* rank){
-	
-	return NULL;
-}
-
-void init_y_array() {
-    y_count = sqrt((double)2*point_count/3) + 1;
-    double delta = (double) 2 / y_count;
-    double y_cur = -1.0;
-
-    ys = calloc(y_count, sizeof(double));
-    for (size_t i = 0; i < y_count; i++) {
-        ys[i] = y_cur;
-        y_cur += delta;
-    }
-}
-
-void init_x_array() {
-    x_count = 1.5 * y_count + 1;
-    double delta = (double) 3 / x_count;
-    double x_cur = -2.0;
-
-    xs = calloc(x_count, sizeof(double));
-    for (size_t i = 0; i < x_count; i++) {
-        xs[i] = x_cur;
-        x_cur += delta;
-    }
-}
+double* x_right_bounds;
+pthread_mutex_t mutex;
 
 void complex_square(struct Point* z) {
     double x = z->x;
@@ -53,66 +30,105 @@ void complex_square(struct Point* z) {
     z->y = 2*x*y;
 }
 
+double get_x_left_bound(long rank) {
+    if (rank == 0) {
+        return (double)X_MIN;
+    }
+    return x_right_bounds[rank - 1];
+}
+
+double random_range(double from, double to) {
+    double range = to - from;
+    double div = RAND_MAX / range;
+    return (double) from + (rand() / div);
+}
+
+void* routine(void* rank){
+	long my_rank = (long) rank;
+    double x_left = get_x_left_bound(my_rank);
+    double x_right = x_right_bounds[my_rank];
+
+    while(cur_point_count < point_count) {
+        double y = random_range(Y_MIN, Y_MAX);
+        double x = random_range(x_left, x_right);
+
+        struct Point z;
+        z.x = 0;
+        z.y = 0;
+        char is_skip = 0;
+
+        printf("rank %d is workin\n", my_rank);
+        for(long i=0; i<ITERATION_COUNT; i++){
+            complex_square(&z);
+            z.x += x;
+            z.y += y;
+            if (z.x * z.x + z.y * z.y > 4) {
+                is_skip = 1;
+                break;
+            }
+        }
+        if (is_skip == 0) {
+            pthread_mutex_lock(&mutex);
+            printf("rank: %d, cur_point: %d\n", my_rank, cur_point_count);
+            if(cur_point_count >= point_count) {
+                printf("End rank: %d\n", my_rank);
+                pthread_mutex_unlock(&mutex);
+                return NULL;
+            }
+            result[cur_point_count].x = x;
+            result[cur_point_count].y = y;
+            cur_point_count++;
+            pthread_mutex_unlock(&mutex);
+        }
+    }
+    printf("End rank: %d\n", my_rank);
+	return NULL;
+}
+
+
+
+void init_x_array() {
+    long x_count = thread_count;
+    double delta = (double) 3 / x_count;
+    double x_cur = (double) X_MIN;
+
+    x_right_bounds = calloc(x_count, sizeof(double));
+    for (size_t i = 0; i < x_count; i++) {
+        x_cur += delta;
+        x_right_bounds[i] = x_cur;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 	thread_count = strtol(argv[1], NULL, 10);
 	point_count = strtol(argv[2], NULL, 10);
 
-
+    cur_point_count = 0;
 	result = calloc(point_count, sizeof(struct Point));
-	//pthread_t* threads = malloc(thread_count * sizeof(pthread_t));
+	pthread_t* threads = malloc(thread_count * sizeof(pthread_t));
+    pthread_mutex_init(&mutex, NULL);
+    init_x_array();
 
     printf("Thread count: %d, point count: %d!\n", thread_count, point_count);
 
     double start_time;
     GET_TIME(start_time);
 
-    init_y_array();
-    init_x_array();
+    // printf("X array with size = %d :", x_count);
+    // for(int i=0; i<x_count; i++){
+    //     printf("%f ", x_right_bounds[i]);
+    // }
+    // printf("\n");
 
-    printf("X array with size = %d :", x_count);
-    for(int i=0; i<x_count; i++){
-        printf("%f ", xs[i]);
-    }
-    printf("\n");
+    for(long i = 0; i < thread_count; i++){
+		pthread_create(&threads[i], NULL, routine, (void*)i);
+	}
 
-    printf("Y array with size = %d :", y_count);
-    for(int i=0; i<y_count; i++){
-        printf("%f ", ys[i]);
-    }
-    printf("\n");
-
-    long cur_points_i = 0;
-    for(long x_i=0; x_i<x_count; x_i++){
-        if (cur_points_i == point_count) {
-                break;
-        }
-        for(long y_i=0; y_i<y_count; y_i++){
-            if (cur_points_i == point_count) {
-                break;
-            }
-            struct Point z;
-            z.x = 0;
-            z.y = 0;
-            char is_skip = 0;
-            for(int i=0; i<4000; i++){
-                complex_square(&z);
-                z.x += xs[x_i];
-                z.y += ys[y_i];
-                if (z.x * z.x + z.y * z.y > 4) {
-                    is_skip = 1;
-                    break;
-                }
-            }
-
-            if (is_skip == 0) {
-                result[cur_points_i].x = xs[x_i];
-                result[cur_points_i].y = ys[y_i];
-                cur_points_i++;
-            }
-
-        }
-    }
+	for (long i = 0; i < thread_count; ++i)
+	{
+		pthread_join(threads[i], NULL);
+	}
 
     double end_time;
     GET_TIME(end_time);
@@ -121,11 +137,10 @@ int main(int argc, char const *argv[])
     {
         printf("Result x=%f y=%f\n", result[i].x, result[i].y);
     }
-    
-
+    printf("Time spent: %f", (end_time - start_time));
     free(result);
-    free(ys);
-    free(xs);
-	//free(threads);
+    free(x_right_bounds);
+    pthread_mutex_destroy(&mutex);
+	free(threads);
 	return 0;
 }
