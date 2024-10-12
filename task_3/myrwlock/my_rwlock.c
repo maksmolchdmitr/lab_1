ff6c8163878945792da966a9d8ff9303a427c219
Nikita Kuritsyn <martyz89@yandex.ru>
2024-10-12 20:26:12 +0300

    fix header

--- task_3/myrwlock/my_rwlock.c
+++ task_3/myrwlock/my_rwlock.c
@@ -1,2 +1,21 @@
 #include "my_rwlock.h"
 
+int my_rwlock_init (my_rwlock* lock) {
+    return 0;
+}
+
+int my_rwlock_destroy (my_rwlock* lock /*out*/){
+    return 0;
+}
+
+int my_rwlock_rdlock (my_rwlock* lock /*in/out*/){
+    return 0;
+}
+
+int my_rwlock_wrlock (my_rwlock* lock /*in/out*/){
+    return 0;
+}
+
+int my_rwlock_unlock (my_rwlock* lock /*in/out*/){
+    return 0;
+}
--- task_3/myrwlock/my_rwlock.h
+++ task_3/myrwlock/my_rwlock.h
@@ -5,21 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct my_rwlock
+
+typedef struct my_rwlock
 {
     pthread_mutex_t mutex;
     pthread_cond_t cond;
     int num_readers_active;
     int num_writers_waiting;
     char writer_active;
-};
+} my_rwlock;
 
 
 
-int my_rwlock_init (pthread_rwlock_t* lock , /*out*/ pthread_rwlockattr_t attr /*in*/); // никита
-int my_rwlock_destroy (pthread_rwlock_t* lock /*out*/);  // макс
-int my_rwlock_rdlock ( pthread_rwlock_t* lock /*in/out*/); //никита
-int my_rwlock_wrlock ( pthread_rwlock_t* lock /*in/out*/); //макс
-int my_rwlock_unlock ( pthread_rwlock_t* lock /*in/out*/);
+int my_rwlock_init (my_rwlock* lock); // никита
+int my_rwlock_destroy (my_rwlock* lock /*out*/);  // макс
+int my_rwlock_rdlock (my_rwlock* lock /*in/out*/); //никита
+int my_rwlock_wrlock (my_rwlock* lock /*in/out*/); //макс
+int my_rwlock_unlock (my_rwlock* lock /*in/out*/);
 
 #endif
